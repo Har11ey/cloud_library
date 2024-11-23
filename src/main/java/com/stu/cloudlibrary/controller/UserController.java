a2db012ada52dab6bb76fd9fe29e76e444884197
ha0 <2653301214@qq.com>
2024-11-24 00:09:54 +0800

    Make some modifications to the login module

--- src/main/java/com/stu/cloudlibrary/controller/UserController.java
+++ src/main/java/com/stu/cloudlibrary/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.stu.cloudlibrary.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.stu.cloudlibrary.model.User;
 import com.stu.cloudlibrary.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +13,18 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     // 用户登录接口
     @PostMapping("/login")
     public String login(@RequestParam String name, @RequestParam String password) {
+        logger.info("用户尝试登录，name: {}, password: {}", name, password); // 这里可能收到合并后的值
         boolean success = userService.login(name, password);
         if (success) {
             return "登录成功";
         }
         return "用户名或密码错误";
     }
+
 
     // 用户注册接口
     @PostMapping("/register")
