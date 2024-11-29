a24c02e29c9071aaa58b9a28323c61eae0b8ffdd
alienhe <753458929@qq.com>
2024-11-29 15:23:13 +0800

    feat: 引入logback打印日志，支持控制台和日志文件输出

--- src/main/java/com/stu/cloudlibrary/CloudlibraryApplication.java
+++ src/main/java/com/stu/cloudlibrary/CloudlibraryApplication.java
@@ -1,9 +1,11 @@
 package com.stu.cloudlibrary;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.mybatis.spring.annotation.MapperScan;
 
+@Slf4j
 @SpringBootApplication
 @MapperScan("com.stu.cloudlibrary.mapper")  // 扫描你的 Mapper 包
 public class CloudlibraryApplication {
